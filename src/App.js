6e58e6a37bce22d335dfcddea5e1ab81c853a621
Jiajin Ou <jiajin.ou@chowbus.com>
2022-01-28 11:37:35 -0600

    test ci_cd

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
-// This site has 3 pages, all of which are rendered
-// dynamically in the browser (not server rendered).
-//
-// Although the page does not ever refresh, notice how
-// React Router keeps the URL up to date as you navigate
-// through the site. This preserves the browser history,
-// making sure things like the back button and bookmarks
-// work properly.
-
 export default function App() {
     return (
         <Router>
@@ -27,14 +18,6 @@ export default function App() {
                 </ul>
 
                 <hr />
-
-                {/*
-          A <Routes> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Routes> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
-        */}
                 <Routes>
                     <Route exact path='/' element={<Home />}></Route>
                     <Route path='/about' element={<About />}></Route>
@@ -44,9 +27,6 @@ export default function App() {
         </Router>
     );
 }
-
-// You can think of these components as "pages"
-// in your app.
 
 function Home() {
     return (
